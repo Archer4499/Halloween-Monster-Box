51690f9e42e4102fc66b3ae2c09c46d6d0961eca
Derek King <king.dm49@gmail.com>
2023-10-16 03:37:57 +0800

    Initial Setup

---
+++ Wokwi-Project/sketch.ino
@@ -0,0 +1,22 @@
+////////    Config    ////////
+
+//// Constants/Settings ////
+// #define DEBUG        // Print debug lines to serial, comment out to disable
+
+#define AUDIO           // Enable the DFPlayer Pro audio player, comment out to disable
+////////
+
+//// Pins ////
+////////
+
+////////    End Config    ////////
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
